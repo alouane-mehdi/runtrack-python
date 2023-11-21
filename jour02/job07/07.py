a4cd93ee73a37810fa6729e3267bf5743acf43ee
mehdi <mehdi.alouane@laplateforme.io>
2023-11-21 17:20:40 +0100

    job 7 pas fini

---
+++ jour02/job07/07.py
@@ -0,0 +1,12 @@
+ligne = 10
+
+alphabet = 65
+i = 0
+
+while(i < ligne):
+    j = 0
+    while(j <=  i):
+        print('%c' %(alphabet + j), end = ' ')
+        j = j + 1
+    print()
+    i = i + 1

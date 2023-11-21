ef5367b05122d09c7e0e5a228a0a77aa232f0cc6
mehdi <mehdi.alouane@laplateforme.io>
2023-11-21 12:07:05 +0100

    Job 5 terminé

---
+++ jour02/job05/05.py
@@ -0,0 +1,14 @@
+
+for lst in list(range(0,100)):
+    if lst%3==0 and lst%5==0:
+        print('fizzbuzz')
+    elif lst%3==0:
+        print('buzz')
+    elif  lst%5==0:
+        print('fizz')          
+    else:
+        print(lst)    
+    
+    
+        
+     

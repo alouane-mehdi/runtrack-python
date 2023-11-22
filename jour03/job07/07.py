7d30d5be1642d337fe9b26c6b8053890350e8628
mehdi <mehdi.alouane@laplateforme.io>
2023-11-22 14:35:21 +0100

    job 7 fini

---
+++ jour03/job07/07.py
@@ -0,0 +1,15 @@
+def message(langage):
+    if langage == "Javascript":
+        return "Tu es un développeur web"
+    if langage == "python":
+        return "tu es un développeur IA"
+    if langage == "java":
+        return "tu es un développeur logiciel"
+    if langage == "reactjs":
+        return "tu es un développeur mobile"
+    else:
+        return "un jour, je serai le meilleur développeur"
+a=message("python15") 
+print(a)   
+    
+    

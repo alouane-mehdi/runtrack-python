c34620cb6907a59234168b8084a586969bf63341
mehdi <mehdi.alouane@laplateforme.io>
2023-11-22 12:08:54 +0100

    Job 5 fini

---
+++ jour03/job05/05.py
@@ -0,0 +1,23 @@
+import string
+
+
+def calcule(num1,operator,num2):
+    if operator == "+":
+        return num1 + num2
+    if operator == "-":
+        return num1 - num2
+    if operator == "/":
+        return num1 / num2
+    if operator == "*":
+        return num1 * num2
+    if operator == "%":
+        return num1 % num2
+    
+a=calcule(50, "+", 8)
+print(a)
+  
+    
+
+
+
+    

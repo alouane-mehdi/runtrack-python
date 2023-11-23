a5e937a2398d5cb3e719a041ce7218dcb5e4e6ee
mehdi <mehdi.alouane@laplateforme.io>
2023-11-23 10:11:00 +0100

    job 11 modif fini

--- jour03/job11/11.py
+++ jour03/job11/11.py
@@ -1,8 +1,7 @@
 def time_to_text(d):
     h = d // 60
     m = d % 60
-    s = m % 60
-    print(d, "min =", h, "h", m, "min", s, "s")
+    print(d, "min =", h, "h", m, "min")
 
-duree_en_minutes = 1585
+duree_en_minutes = 2000
 time_to_text(duree_en_minutes)
